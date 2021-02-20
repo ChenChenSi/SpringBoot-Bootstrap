ec348ae17a5a292c0755b1365e352805fc8516ad
names <49894120+ChenChenSi@users.noreply.github.com>
2021-02-21 02:43:44 +0800

    'test-2'

--- src/main/java/com/luojay/springbootthymeleafbootstrapui/SpringbootThymeleafBootstrapuiApplication.java
+++ src/main/java/com/luojay/springbootthymeleafbootstrapui/SpringbootThymeleafBootstrapuiApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootThymeleafBootstrapuiApplication {
-
+///llllll1
     public static void main(String[] args) {
         SpringApplication.run(SpringbootThymeleafBootstrapuiApplication.class, args);
     }
